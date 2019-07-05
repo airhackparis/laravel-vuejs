3cb6e8760e7eafd14d6513f45114ca8c61a0da76
Romain Malnoult <romain.malnoult@gmail.com>
2019-07-05 17:57:59 +0200

    updated frontend boilerplate

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+import store from './store'
+import router from './router'
+
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -19,7 +22,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('hello-world', require('./components/HelloWorld.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +32,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    store,
+    router,
+    template: `<router-view/>`,
 });
