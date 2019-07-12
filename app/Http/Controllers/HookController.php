0f9459da9be861ef145ac3941b12df512b2f454f
Pauline Huguenel <paulineh@theodo.fr>
2019-07-12 22:29:05 +0200

    :beers: Create and sort tasks array

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -5,13 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
+
 class HookController extends Controller
 {
+    function date_sort($task1, $task2)
+    {
+        return strtotime($task1["dueTime"]) - strtotime($task2["dueTime"]);
+    }
+
     public function incomingTasks(Request $request)
     {
-        Log::error($request->all());
+        $result = $request->all();
+        $taskersCount = $result["taskersCount"];
+        $taskers = range(1, 50);
 
-        return response()->json(['success' => true]);
+        $tasks = $result["tasks"];
+        usort($tasks, array($this, "date_sort"));
+        $orderedTaskIds = array_map(function($task) {return $task["id"];}, $tasks);
+        $tasksDict = array_combine($orderedTaskIds,$tasks);
+
+        array_walk($tasksDict, array($this, "is_executable"));
+
+        return response()->json($tasksDict);
     }
 
     public function health()
